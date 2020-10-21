8d0976fbf8aa0568eda428b265c958300a3f5033
Sriram Sivarajah <sriramsivarajah@gmail.com>
2020-10-21 17:20:24 +0100

    adding basic functions to map component

--- frontend/src/components/Map.js
+++ frontend/src/components/Map.js
@@ -1,16 +1,34 @@
 import React from 'react'
 import MapGL from '@urbica/react-map-gl'
+import 'mapbox-gl/dist/mapbox-gl.css'
 
 class Map extends React.Component {
+
+  state = {
+    viewport: {
+      zoom: 1,
+      latitude: 0,
+      longitude: 0
+    },
+    bounds: null
+  }
+
+  setViewport = newViewport => {
+    const viewport = { ...this.state.viewport, ...newViewport }
+    const bounds = this.mapRef ? { ...this.mapRef.getMap().getBounds() } : null
+    this.setState({ viewport, bounds })
+  }
 
   render() {
     return (
       <MapGL
         ref={map => this.mapRef = map}
-        mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_ACCESS_TOKEN}
         mapStyle='mapbox://styles/mapbox/streets-v11'
-        width="100%" height="100%"
-        getCursor={() => 'arrow'}
+        style={{ width: '100%', height: '400px' }}
+        cursorStyle="default"
+        accessToken={process.env.REACT_APP_MAPBOX_ACCESS_TOKEN}
+        {...this.state.viewport}
+        onViewportChange={this.setViewport}
       />
     )
   }
