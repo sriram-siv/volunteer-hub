45fa0f05c62c3fad23a2c341d92686945b475ced
Sriram Sivarajah <sriramsivarajah@gmail.com>
2020-10-21 22:33:34 +0100

    adding basic chat window for testing

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -2,11 +2,20 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 class Home extends React.Component {
+
+  state = {
+    roomName: ''
+  }
+
+  handleChange = event => {
+    this.setState({ [event.target.name]: event.target.value })
+  }
+
   render() {
     return (
       <>
-        <input type="text" />
-        <Link to="/chat/my_room" >Go</Link>
+        <input type="text" name="roomName" value={this.state.roomName} onChange={this.handleChange} />
+        <Link to={`/chat/${this.state.roomName}`} >Go</Link>
       </>
     )
   }
--- frontend/src/components/Room.js
+++ frontend/src/components/Room.js
@@ -2,12 +2,13 @@ import React from 'react'
 
 class Room extends React.Component {
 
+  state = {
+    messages: [],
+    draft: ''
+  }
+
   componentDidMount = () => {
     this.connectToSocket()
-    
-
-
-
   }
 
   connectToSocket = () => {
@@ -20,24 +21,37 @@ class Room extends React.Component {
     this.chatSocket.onmessage = (e) => {
       const data = JSON.parse(e.data)
       console.log(data)
+      this.setState({ messages: [...this.state.messages, data.message ] })
     }
 
     this.chatSocket.onclose = () => console.error('Chat socket closed unexpectedly')
     console.log(this.chatSocket)
   }
 
-  sendMessage = () => {
+  sendMessage = event => {
+    event.preventDefault()
     this.chatSocket.send(JSON.stringify({
-      'text': 'hello',
+      'text': this.state.draft,
       'user': 'volunteer_55'
     }))
+    this.setState({ draft: '' })
   }
 
+  handleChange = event => {
+    this.setState({ [event.target.name]: event.target.value })
+  }
   render() {
+    const { messages, draft } = this.state
     return (
-      <div>
-        <button onClick={this.sendMessage} >click</button>
-      </div>
+      <>
+        <div style={{ backgroundColor: 'plum', height: '200px', overflowY: 'scroll' }}>
+          {messages.map((message, i) => <p key={i}>{message.text}</p>)}
+        </div>
+        <form style={{ marginTop: '5px' }}>
+          <input type="text" style={{ display: 'inline-block', marginLeft: '5px', paddingTop: '3px', width: 'calc(100% - 120px)' }} className="form-control" name="draft" value={draft} onChange={this.handleChange} />
+          <button style={{ width: '100px', marginLeft: '10px' }} className="btn btn-outline-primary" onClick={this.sendMessage} >send</button>
+        </form>
+      </>
     )
   }
 }
