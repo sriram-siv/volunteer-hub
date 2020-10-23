494b27fae79d8b65311c622aa7a10b13db4bfba0
dongra02 <dsgraham1@gmail.com>
2020-10-23 19:02:22 +0300

    422 on user registration api. form and stat working, issue posting to backend

--- frontend/src/components/common/DgTest.js
+++ frontend/src/components/common/DgTest.js
@@ -2,6 +2,7 @@ import React from 'react'
 
 import InputText from '../elements/InputText'
 import Button from '../elements/Button'
+import { registerUser } from '../../lib/api'
 
 // TEST REGISTRATION FORM
 
@@ -10,8 +11,11 @@ class DgTest extends React.Component {
     formData: {
       firstName: null,
       lastName: null,
+      userName: null,
       email: null,
-      phone: null
+      phone: null,
+      password: null,
+      confirmPassword: null
     }
   }
 
@@ -23,10 +27,15 @@ class DgTest extends React.Component {
     this.setState({ formData })
   }
 
-  handleSubmit = (e) => {
+  handleSubmit = async (e) => {
     e.preventDefault()
-    // just add in api route to register
     console.log(this.state.formData)
+    try {
+      const response = await registerUser(this.state.formData)
+      console.log(response.data.message)
+    } catch (err) {
+      console.log(err)
+    }
   }
 
   render() {
@@ -35,8 +44,11 @@ class DgTest extends React.Component {
         <form>
           <InputText label='First Name' name='firstName' returnValue={this.handleChange}/>
           <InputText label='Last Name' name='lastName' returnValue={this.handleChange}/>
+          <InputText label='Username' name='userName' returnValue={this.handleChange}/>
           <InputText label='Email' name='email' returnValue={this.handleChange}/>
           <InputText label='Phone' name='phone' returnValue={this.handleChange}/>
+          <InputText label='Password' name='password' returnValue={this.handleChange}/>
+          <InputText label='Confirm Password' name='confirmPassword' returnValue={this.handleChange}/>
           <Button width='100%' label='submit' onClick={this.handleSubmit}/>
         </form>
       </div>
--- jwt_auth/models.py
+++ jwt_auth/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
+    username=models.CharField(max_length=30, unique=True)
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     email = models.EmailField()
