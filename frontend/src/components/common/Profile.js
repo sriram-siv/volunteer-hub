763f8838c189488a9749f1bc77ccc20376cd1f69
dongra02 <dsgraham1@gmail.com>
2020-10-28 11:29:17 +0200

    edit user data functional but not linked to backend

--- frontend/src/components/common/Profile.js
+++ frontend/src/components/common/Profile.js
@@ -3,6 +3,8 @@ import styled from 'styled-components'
 import Select from 'react-select'
 
 import BannerImage from '../elements/BannerImage'
+import Button from '../elements/Button'
+import InputText from '../elements/InputText'
 
 import { getSingleProfile, getAllSkills } from '../../lib/api'
 import Schedule from '../elements/Schedule'
@@ -44,11 +46,20 @@ class Profile extends React.Component {
       phone: null,
       profile_image: null
     },
+    pendingUserData: {
+      username: null,
+      first_name: null,
+      last_name: null,
+      email: null,
+      phone: null,
+      profile_image: null
+    },
     skills: null,
     formData: {
       user_skills: null,
       schedule: Array.from({ length: 14 }).fill(false)
-    }
+    },
+    editMode: false
   }
 
   componentDidMount = async () => {
@@ -71,7 +82,30 @@ class Profile extends React.Component {
     
     const formData = { ...this.state.formData, user_skills: response.data.user_skills }
     
-    this.setState({ userData, formData }, () => console.log(this.state.formData)) 
+    this.setState({ userData, pendingUserData: userData, formData }, () => console.log(this.state.formData)) 
+  }
+
+  handleEditMode = () => {
+    this.setState({ editMode: !this.state.editMode })
+  }
+
+  handleEditChange = (event) => {
+    const pendingUserData = {
+      ...this.state.pendingUserData,
+      [event.target.name]: event.target.value
+    }
+    this.setState({ pendingUserData })
+  }
+
+  saveEdits = () => {
+    console.log('time to save edits to db')
+    
+    this.handleEditMode()
+  }
+
+  discardEdits = () => {
+    this.setState({ pendingUserData: this.state.userData })
+    this.handleEditMode()
   }
 
   getSkills = async () => {
@@ -98,7 +132,7 @@ class Profile extends React.Component {
 
   render() {
     const { app } = this.props
-    const { userData, skills } = this.state
+    const { userData, pendingUserData, skills, editMode } = this.state
     const { schedule } = this.state.formData
 
     const selectStyles = {
@@ -132,10 +166,25 @@ class Profile extends React.Component {
         <Select value={user_skills} options={skills} isMulti onChange={this.editSkills}/>
         <button onClick={app.logout}>Logout</button>
         <button>save</button>
-        <MemberDetail>{userData.first_name}</MemberDetail>
-        <MemberDetail>{userData.last_name}</MemberDetail>
-        <MemberDetail>{userData.email}</MemberDetail>
-        <MemberDetail>{userData.phone}</MemberDetail>
+        {!editMode &&
+          <div>
+            <MemberDetail>{userData.first_name}</MemberDetail>
+            <MemberDetail>{userData.last_name}</MemberDetail>
+            <MemberDetail>{userData.email}</MemberDetail>
+            <MemberDetail>{userData.phone}</MemberDetail>
+            <Button onClick={this.handleEditMode} label='Edit'></Button>
+          </div>}
+        {editMode &&
+          <div>
+            <InputText name='first_name' value={pendingUserData.first_name} label='First Name'  returnValue={this.handleEditChange}></InputText>
+            <InputText name='last_name' value={pendingUserData.last_name} label='Last Name' returnValue={this.handleEditChange}></InputText>
+            <InputText name='email' value={pendingUserData.email} label='Email' returnValue={this.handleEditChange}></InputText>
+            <InputText name='phone' value={pendingUserData.phone} label='Phone' returnValue={this.handleEditChange}></InputText>
+            <Button onClick={this.saveEdits} label='Save'></Button>
+            <Button onClick={this.discardEdits} label='Cancel'></Button>
+          </div>
+
+        }
       </Wrapper>
     )
   }
