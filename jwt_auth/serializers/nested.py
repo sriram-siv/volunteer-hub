a5c3f84ac1a8dc4b597a51ca5712d9d280f63334
dongra02 <dsgraham1@gmail.com>
2020-10-28 12:56:12 +0200

    updated nested user serializer for shifts/skills populate

--- jwt_auth/serializers/nested.py
+++ jwt_auth/serializers/nested.py
@@ -1,11 +1,12 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
+from .populated import PopulatedUserSerializer
 
 User = get_user_model()
 
-class NestedUserSerializer(serializers.ModelSerializer):
+class NestedUserSerializer(PopulatedUserSerializer):
 
     class Meta:
         model = User
-        fields = ('id', 'last_name', 'first_name', 'username')
+        fields = ('id', 'last_name', 'first_name', 'username', 'profile_image', 'user_skills', 'user_shifts')
         
