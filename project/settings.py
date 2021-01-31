03d4142fe1adb83c2ea52c79aea458f42c1b7bba
Sriram Sivarajah <sriramsivarajah@gmail.com>
2021-01-31 20:20:07 +0000

    a

--- project/settings.py
+++ project/settings.py
@@ -170,7 +170,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'frontend', "build", "static"),
+    os.path.join(BASE_DIR, "frontend", "build", "static"),
 )
 
 django_on_heroku.settings(locals())
