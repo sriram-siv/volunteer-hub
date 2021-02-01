e6f01199f0a8d9a4410bb92863d1d792988a84ba
Sriram Sivarajah <sriramsivarajah@gmail.com>
2021-02-01 11:21:25 +0000

    reactivating sockets for chat rooms

---
+++ frontend/build/precache-manifest.9f3bab1e3dff5e9225fc1ff02561d96e.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7ea00fb342dd2e51687dbe002424464",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3884326844b919958377",
+    "url": "/static/css/2.08c12e69.chunk.css"
+  },
+  {
+    "revision": "b32eb3c05817c7408f02",
+    "url": "/static/css/main.cbf4965d.chunk.css"
+  },
+  {
+    "revision": "3884326844b919958377",
+    "url": "/static/js/2.c3c8247e.chunk.js"
+  },
+  {
+    "revision": "4201385ef9445f2cfbb8c74b2693dc3c",
+    "url": "/static/js/2.c3c8247e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b32eb3c05817c7408f02",
+    "url": "/static/js/main.ecf20dcc.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  },
+  {
+    "revision": "6174eea3d0f4e5faa1ae1e60835436d1",
+    "url": "/static/media/campPreview.6174eea3.jpg"
+  },
+  {
+    "revision": "39815fad65d3e1d14b5dff3a2b22d28e",
+    "url": "/static/media/campaigns.39815fad.jpg"
+  },
+  {
+    "revision": "86166eee501f86cd3a70aecb49fd49d9",
+    "url": "/static/media/chatgif.86166eee.gif"
+  },
+  {
+    "revision": "24aea1873e4baa05ac42d234107cfa5e",
+    "url": "/static/media/default_banner.24aea187.png"
+  },
+  {
+    "revision": "6102e870b99a3bf30ca39d9c0a6fd114",
+    "url": "/static/media/default_banner_profile.6102e870.jpg"
+  },
+  {
+    "revision": "d5e6b69a7eb6ce97840f4f8979673413",
+    "url": "/static/media/default_profile.d5e6b69a.png"
+  }
+]);
