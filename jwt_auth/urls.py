838c5d0d99e20c1e2cdd7c617eb1dcf33c47cfb7
Sriram Sivarajah <sriramsivarajah@gmail.com>
2021-02-02 15:29:51 +0000

    fixed import

--- jwt_auth/urls.py
+++ jwt_auth/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import RegisterView, LoginView, ProfileListView, ProfileDetailView, ProfileSkillsView, ProfileShiftView, AuthToken
+from .views import RegisterView, LoginView, ProfileListView, ProfileDetailView, ProfileSkillsView, ProfileShiftView
 
 urlpatterns = [
     path('register/', RegisterView.as_view()),
