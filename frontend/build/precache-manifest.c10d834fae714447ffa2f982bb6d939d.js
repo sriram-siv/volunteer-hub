d2bff85eaed17d2171f19b0956d195a88dc1728d
Sriram Sivarajah <sriramsivarajah@gmail.com>
2021-02-02 15:34:38 +0000

    revert to working ver

---
+++ frontend/build/precache-manifest.c10d834fae714447ffa2f982bb6d939d.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffb695486f61e89f885470e23ef91f70",
+    "url": "/index.html"
+  },
+  {
+    "revision": "63d7341a89cb6659535f",
+    "url": "/static/css/2.08c12e69.chunk.css"
+  },
+  {
+    "revision": "f3c546a9831e6baf89d3",
+    "url": "/static/css/main.cbf4965d.chunk.css"
+  },
+  {
+    "revision": "63d7341a89cb6659535f",
+    "url": "/static/js/2.d038370b.chunk.js"
+  },
+  {
+    "revision": "4201385ef9445f2cfbb8c74b2693dc3c",
+    "url": "/static/js/2.d038370b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3c546a9831e6baf89d3",
+    "url": "/static/js/main.7c20f34a.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  },
+  {
+    "revision": "6174eea3d0f4e5faa1ae1e60835436d1",
+    "url": "/static/media/campPreview.6174eea3.jpg"
+  },
+  {
+    "revision": "39815fad65d3e1d14b5dff3a2b22d28e",
+    "url": "/static/media/campaigns.39815fad.jpg"
+  },
+  {
+    "revision": "86166eee501f86cd3a70aecb49fd49d9",
+    "url": "/static/media/chatgif.86166eee.gif"
+  },
+  {
+    "revision": "24aea1873e4baa05ac42d234107cfa5e",
+    "url": "/static/media/default_banner.24aea187.png"
+  },
+  {
+    "revision": "6102e870b99a3bf30ca39d9c0a6fd114",
+    "url": "/static/media/default_banner_profile.6102e870.jpg"
+  },
+  {
+    "revision": "d5e6b69a7eb6ce97840f4f8979673413",
+    "url": "/static/media/default_profile.d5e6b69a.png"
+  }
+]);
