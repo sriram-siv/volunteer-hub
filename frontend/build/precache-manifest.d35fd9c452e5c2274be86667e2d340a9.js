79a324fb1cbbbfc0573137b851039fba2755e840
Sriram Sivarajah <sriramsivarajah@gmail.com>
2021-03-18 10:31:30 +0000

    ignore file

---
+++ frontend/build/precache-manifest.d35fd9c452e5c2274be86667e2d340a9.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fe91d33f862a77876d771cf9ae65612",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6a339f196e87debf3f01",
+    "url": "/static/css/2.08c12e69.chunk.css"
+  },
+  {
+    "revision": "4063d8ce66cb4920d0ec",
+    "url": "/static/css/main.7b5198f8.chunk.css"
+  },
+  {
+    "revision": "6a339f196e87debf3f01",
+    "url": "/static/js/2.8b93a593.chunk.js"
+  },
+  {
+    "revision": "4201385ef9445f2cfbb8c74b2693dc3c",
+    "url": "/static/js/2.8b93a593.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4063d8ce66cb4920d0ec",
+    "url": "/static/js/main.2018436d.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  },
+  {
+    "revision": "24aea1873e4baa05ac42d234107cfa5e",
+    "url": "/static/media/default_banner.24aea187.png"
+  },
+  {
+    "revision": "6102e870b99a3bf30ca39d9c0a6fd114",
+    "url": "/static/media/default_banner_profile.6102e870.jpg"
+  },
+  {
+    "revision": "d5e6b69a7eb6ce97840f4f8979673413",
+    "url": "/static/media/default_profile.d5e6b69a.png"
+  },
+  {
+    "revision": "32b5f2c07d16527db3507e35bb1c982c",
+    "url": "/static/media/screenshot_main.32b5f2c0.png"
+  }
+]);
